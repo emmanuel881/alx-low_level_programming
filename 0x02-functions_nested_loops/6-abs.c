5e019aa8f911fb0e96822bec172847e70705be71
Emmanuel Saura <emmanuelsaura007@gmail.com>
2022-10-21 06:01:14 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - absolute value
+ *
+ * @n: The int print
+ * Return: 0
+ */
+int _abs(int n)
+{
+	if (n <= 0)
+	{
+		return (n *(-1));
+	}
+	else
+	{
+		return (n);
+	}
+}
