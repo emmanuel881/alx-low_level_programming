6cce9bf7cd53fdf4e3c80aa30831bb83f7f204a3
Emmanuel Saura <emmanuelsaura007@gmail.com>
2022-10-21 01:55:09 -0700

    is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - short description, single line
+ * @c: contains value to be compared
+ * Return: 1 if true and 0 if false
+ */
+int _islower(int c)
+{
+	if (c > 'a' && c < 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
