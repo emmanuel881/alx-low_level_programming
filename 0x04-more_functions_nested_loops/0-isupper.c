74fc9147c79384662452069633e07e49b93f897d
Emmanuel Saura <emmanuelsaura007@gmail.com>
2022-10-24 00:56:01 -0700

    my commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - evalute is letter is uppercase
+ * @c: print int
+ *
+ * Return: Always 0
+ */
+
+int _isupper(int c)
+{
+	if ( c > 64 && c < 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
