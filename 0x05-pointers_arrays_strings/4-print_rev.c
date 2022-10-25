3ec2dde11dc356f986b5909a401beda493ffae6d
Emmanuel Saura <emmanuelsaura007@gmail.com>
2022-10-25 12:34:09 -0700

    my commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - print string in reverse order
+ * @s: string pointer
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int size;
+
+	size = 0;
+	while (*(s + size) != '\0')
+	{
+		size++;
+	}
+	while (*(s + size) >= 0)
+	{
+		_putchar(*(s + size));
+	}
+}
