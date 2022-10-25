738700e02408404f1b3bf73f61eaaa3a7e3a1b29
Emmanuel Saura <emmanuelsaura007@gmail.com>
2022-10-25 13:11:14 -0700

    my commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -14,8 +14,9 @@ void print_rev(char *s)
 	{
 		size++;
 	}
-	while (*(s + size) >= 0)
+	for (size = size - 1; i >= 0; i--)
 	{
-		_putchar (*(s + size));
+		_putchar(*(s + size));
 	}
+	_putchar('\n');
 }
