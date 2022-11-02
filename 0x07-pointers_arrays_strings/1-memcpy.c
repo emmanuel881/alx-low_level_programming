09abaac5a272df715940f71fe771a46c2b6b193e
Emmanuel Saura <emmanuelsaura007@gmail.com>
2022-11-02 07:24:55 -0700

    my commit

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy - copies memory area
+ * @n: number of bytes to copy
+ * @src: memory area to copy from
+ * @dest: where to copy to
+ *
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(src + i) = *(dest + i);
+	}
+	return (dest);
+}
