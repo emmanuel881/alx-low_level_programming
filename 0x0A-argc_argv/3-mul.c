b10a62871b39674e47863c9ae0ee42af2752b548
Emmanuel Saura <emmanuelsaura007@gmail.com>
2022-11-07 10:48:09 -0800

    multi

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -multiplies two arguments
+ * @argc: number of argumments
+ * @argv: string of arguments
+ *
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	int mult;
+
+	if (argc > 1 && argc <= 3)
+	{
+		mult = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mult);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
