12168b41455b4f53416f4148d6f1095962504add
Emmanuel Saura <emmanuelsaura007@gmail.com>
2022-11-09 08:29:33 -0800

    my commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - creates a copy aof a string
+ * @str: pointer to a string
+ * 
+ * Return: 0
+ */
+int _strlen(char *str)
+{
+	int i;
+	
+	i = 0;
+	while (*str != '\0')
+	{
+		str++;
+		i++;
+	}
+	return (i);
+}
+char *_strdup(char *str)
+{
+	int len, i;
+	char *str2;
+	
+	len = _strlen(str);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	str2 = malloc(sizeof(char) * len);
+	for (i = 0; i < len; i++)
+	{
+		str2[i] = str[i];
+	}
+	return (str2);
+}
