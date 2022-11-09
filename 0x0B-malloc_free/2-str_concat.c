443b4255132e6228dec56d5628e0d0b1a36096fb
emmanuel254 <80281963+emmanuel881@users.noreply.github.com>
2022-11-09 20:03:14 +0300

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,23 @@
 #include "main.h"
 /**
+ * _strlen - get string length
+ * @str: pointer to string
+ *
+ * Return: 0
+ */
+int _strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (*str != '\0')
+	{
+		str++;
+		i++;
+	}
+	return (i);
+}
+/**
  * str_concat - concatinates two strings
  * @s1: string one
  * @s2: string two
