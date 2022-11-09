16f4aed955ea5880b661fdb8528cab886f1d442c
Emmanuel Saura <emmanuelsaura007@gmail.com>
2022-11-09 08:59:45 -0800

    my commi

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * str_concat - concatinates two strings
+ * @s1: string one
+ * @s2: string two
+ *
+ * Return: pointer to the concatinated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+
