9746eea4a6e658036a515f90c1d1d329258d5ce1
Emmanuel Saura <emmanuelsaura007@gmail.com>
2022-11-15 11:19:17 -0800

    y commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - create structure for dogs
+ * @name: name of the dog
+ * @age: dog's age
+ * @owner: name of owner
+ *
+ * Description: Longer description
+ */
+typedef struct
+{
+	char *name;
+	float age;
+	char *owner;
+}dog;
+
