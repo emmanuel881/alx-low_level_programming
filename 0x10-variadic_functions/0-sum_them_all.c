d1f111553052d1dc5f334930af67e9a16b2383aa
Emmanuel Saura <emmanuelsaura007@gmail.com>
2022-11-22 03:20:26 -0800

    my commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+/**
+ * sum_them_all - sums up integer arguments
+ * @n: number of arguments
+ *
+ * Return: sum of arguments
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum;
+	int i;
+	int extract;
+	va_list list;
+
+	sum = 0;
+	if (n == 0)
+	{
+		return (0);
+	}
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		extract = va_arg(list, int);
+		sum = sum + extract;
+	}
+	va_end(list);
+	return (sum);
+}
