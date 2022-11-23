4ac630d923bc898cf564c93ba580e7f498fd6cc7
Emmanuel Saura <emmanuelsaura007@gmail.com>
2022-11-22 23:52:28 -0800

    my commit

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	int extract;
+	va_list list;
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+	{
+		extract = va_arg(list, int);
+		printf("%d", extract);
+		if (i != (n - 1) && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+}
