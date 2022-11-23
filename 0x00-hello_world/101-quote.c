b4d68f2bc95230bcfb9f8f2a1855d7078068b952
Emmanuel Saura <emmanuelsaura007@gmail.com>
2022-11-23 01:21:27 -0800

    my commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,9 +8,8 @@
  */
 int main(void)
 {
-	char mes[60];
+	char m[60] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-	mes = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	write(STDOUT_FILENO, mes, 59);
+	write(STDOUT_FILENO, m, 59);
 	return (1);
 }
