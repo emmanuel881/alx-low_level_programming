943ff02a9bb78bee0bee0672f0e6499530797e3c
Emmanuel Saura <emmanuelsaura007@gmail.com>
2022-11-23 01:06:48 -0800

    my commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
-int main()
+/**
+ * main - prints to  standard error
+ *
+ * Return: 0
+ */
+int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	char mes[60];
+
+	mes = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	write(STDOUT_FILENO, mes, 59);
 	return (1);
 }
